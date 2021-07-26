e039357ad905aa9378b8cd50f43711b795d194cc
Marcus <marcusvmelodev@gmail.com>
2021-07-26 13:54:52 -0300

    Added methods to user class

--- course-manager/src/app/user.ts
+++ course-manager/src/app/user.ts
@@ -8,4 +8,18 @@ export class User {
         this.email = "";
         this.hashedpsw = 0;
     }
+
+    copy(): User {
+        var copiedUser: User = new User();
+        copiedUser.nome = this.nome;
+        copiedUser.email = this.email;
+        copiedUser.hashedpsw = this.hashedpsw;
+        return copiedUser; 
+    }
+
+    set_user_data(nome: string, email: string, hashedpsw: number): void {
+        this.nome = nome;
+        this.email = email;
+        this.hashedpsw = hashedpsw;
+    }
 }
