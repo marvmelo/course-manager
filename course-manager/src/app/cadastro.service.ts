3c0b777a35b6a604c66fc6ce225355204389d5a3
Marcus <marcusvmelodev@gmail.com>
2021-07-28 14:47:38 -0300

    Added sendNewUse method to cadastro service

--- course-manager/src/app/cadastro.service.ts
+++ course-manager/src/app/cadastro.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CadastroService {
 
-  constructor() { }
+  private serverUrl: string = "http://localhost:3000/"
+
+  constructor(private http: HttpClient) { }
+
+  sendNewUser(user: User): Promise<User>{
+    return this.http.post<User>(this.serverUrl + "/createAccount", JSON.stringify(user), {observe: 'body'})
+    .toPromise();
+  }
+
 }
