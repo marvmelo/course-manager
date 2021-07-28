59406747d8eae929ef8b9ee77a837832075028aa
Marcus <marcusvmelodev@gmail.com>
2021-07-28 13:23:11 -0300

    Created hashstring function in hashing service

--- course-manager/src/app/hashing.service.ts
+++ course-manager/src/app/hashing.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as hasha from 'hasha';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class HashingService {
 
   constructor() { }
+
+  HashString(text: string): string {
+    return hasha(text);
+  }
 }
