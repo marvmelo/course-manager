8400ec69f8d2a342fb1549997d864a667e84dced
Marcus <marcusvmelodev@gmail.com>
2021-07-29 14:07:28 -0300

    Comments cadastro service

--- course-manager/src/app/cadastro.service.ts
+++ course-manager/src/app/cadastro.service.ts
@@ -7,10 +7,13 @@ import { User } from './user';
 })
 export class CadastroService {
 
+  // Endereço do servidor temporário para testes
   private serverUrl: string = "http://localhost:3000/"
 
   constructor(private http: HttpClient) { }
 
+  // Essa função pegar um usuário e manda para http://localhost:3000/createAccount com uma requisição POST
+  // Ela retorna uma promesa, que pode ser um User ou não em casos de sucesso e erro, respectivamente
   sendNewUser(user: User): Promise<User>{
     return this.http.post<User>(this.serverUrl + "createAccount", user)
     .toPromise();
