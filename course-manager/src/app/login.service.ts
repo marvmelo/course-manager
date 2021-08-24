d10c2e10e9e34e9b40132eb05a7bc562a8594eb7
Marcus <marcusvmelodev@gmail.com>
2021-08-24 18:54:42 -0300

    Login service sends request to server

--- course-manager/src/app/login.service.ts
+++ course-manager/src/app/login.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private serverUrl: string = "http://localhost:3000/"
+
+  constructor(private http: HttpClient) { }
+
+  confirmUser(user: User): Promise<string>{
+    return this.http.post<string>(this.serverUrl + "login", user, {responseType: 'text' as 'json' , observe: 'body'})
+    .toPromise()
+  }
 }
