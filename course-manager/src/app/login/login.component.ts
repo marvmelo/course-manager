904bf11d934b80965aed80f0167db0b4e02fe12a
Marcus <marcusvmelodev@gmail.com>
2021-08-24 17:46:09 -0300

    Login backbone set

--- course-manager/src/app/login/login.component.ts
+++ course-manager/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  email: string = "";
+  psw: string = "";
+
+
 
   ngOnInit(): void {
   }
